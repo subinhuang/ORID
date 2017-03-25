29a9ffa114b8e071191d6292a4033afc22b1b38c
subinhuang <806519191@qq.com>
2017-03-25 16:34:56 +0800

    using scope to write order by created_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,35 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
+
   def new
     @group = Group.new
   end
+
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts.recent
   end
+
   def edit
-    @group = Group.find(params[:id])
+
   end
+
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path, notice: "update sucess"
     else
       render :edit
-    end 
+    end
   end
+
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
     if @group.save
       redirect_to groups_path
@@ -28,14 +37,23 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
   def group_params
     params.require(:group).permit(:title, :description)
 
