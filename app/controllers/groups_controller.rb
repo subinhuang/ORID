bdaeee67fa701edbc4e283c96d7836ae38c10b8c
subinhuang <806519191@qq.com>
2017-03-25 15:31:07 +0800

    complete CRUD

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,38 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+  def new
+    @group = Group.new
+  end
+  def show
+    @group = Group.find(params[:id])
+  end
+  def edit
+    @group = Group.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "update sucess"
+  end
+  def create
+    @group = Group.new(group_params)
+    @group.save
+    redirect_to groups_path
+  end
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :orids
-  root 'orids#index'
+  resources :groups
+  root 'groups#index'
+  # root 'orids#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
