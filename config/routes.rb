c96652a559047a59e33bbc5833b456da9c0e21cc
subinhuang <806519191@qq.com>
2017-03-26 09:06:19 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :orids
   resources :groups do
     resources :posts
+    member do
+      post :join
+      post :quit
+    end
+  end
+  resources :posts
+  namespace :account do
+    resources :groups
   end
     root 'groups#index'
   # root 'orids#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325082755) do
+ActiveRecord::Schema.define(version: 20170325114546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
